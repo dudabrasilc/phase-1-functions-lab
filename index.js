6e5f10dc8aa2c5042536345353c89786da5c3538
Maria Coitinho <brasildu@gmail.com>
2022-06-30 14:48:22 -0500

    commit tree 8e9575f3f452b15025352e1ad4f8d56901c1e21b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(stNumber){
+    return Math.abs(stNumber - 42);
+}
+
+function distanceFromHqInFeet(stNumber){
+   return distanceFromHqInBlocks(stNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    let distInBlocks = Math.abs(destination - start);
+    let distInFeet = distInBlocks * 264;
+    return distInFeet;
+}
+
+
+function calculatesFarePrice(start, destination){
+    let distInFeet1 = distanceTravelledInFeet(start, destination);
+    if (distInFeet1 < 400) {
+        return 0;
+    } else if (distInFeet1 >=400 && distInFeet1 <= 2000) {
+        let freeSample = distInFeet1 - 400;
+        return freeSample * 0.02;
+    } else if (distInFeet1 > 2000 && distInFeet1 <= 2500) {
+        return 25;
+    } else if (distInFeet1 > 2500) {
+        return 'cannot travel that far';
+    }
+}
